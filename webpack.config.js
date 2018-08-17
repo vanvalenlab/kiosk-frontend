a962e5176688b3be4d5440616057641081e93561
bchang1213 <brian@podiumbrand.com>
2018-08-17 13:50:39 -0700

    edited gitignore to exclude the .env file. changed project name in package.json. attempted to make some unsuccessful changs to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const dotenv = require('dotenv');
 
 module.exports = {
 	entry: "./src/index.js",
