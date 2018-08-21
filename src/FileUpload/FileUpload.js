06881e7c539e3c3e949937774523d09a522598e7
bchang1213 <brian@podiumbrand.com>
2018-08-21 09:32:42 -0700

    altered webpack config into a function, added a function to parse .env file and assign it to webpack plugin. referenced the env variables in the fileupload component. created a .env.test file.

--- src/FileUpload/FileUpload.js
+++ src/FileUpload/FileUpload.js
@@ -19,23 +19,33 @@ export default class FileUpload extends Component{
 		super();
 		this.state = {
 			file: [],
-			uploadedFileLocation: null,
+			uploadedS3FileName: null,
+			uploadedFileLocation: null
 		}
 	}
-
+	/*
+		Example s3 response json:
+		{
+			"bucket":"deepcell-output",
+			"key":"dna_version2.tif",
+			"location":"https://deepcell-output.s3.amazonaws.com/dna_version2.tif",
+			"result":{}
+		}
+	*/
 	//This function will run upon file upload completion.
 	onDrop(droppedfile){
 		console.log("Accepted Files: " + JSON.stringify(droppedfile));
 		//set the component state with the uploaded files
 		this.setState({file: droppedfile});
 		//for each image that was selected by the file upload,
-		this.state.file.map(f => 
+		this.state.file.map(f =>
 			//Upload files to AWS S3 bucket
 			S3Client
 				.uploadFile(f, config)
 				.then(data => {
-					console.log("Upload to s3 bucket was successful: " + data);
+					console.log("Upload to s3 bucket was successful: " + JSON.stringify(data));
 					//set the uploadedFileLocation state to the s3 bucket image location that was just uploaded.
+					this.setState({uploadedS3FileName: f.name})
 					this.setState({uploadedFileLocation: data.location});
 					//submit the URL to the predictImage() function, detailed below.
 					this.predictImage();
@@ -47,16 +57,21 @@ export default class FileUpload extends Component{
 	//RUN TO POST S3UPLOAD INFORMATION TO THE FLASK API
 	predictImage(){
 		console.log("Sending uploaded image's S3 Bucket URL to the Flask API...");
-
-		axios.post("http://localhost:1337/predict/default/0", {
-			"instances": [
-				{
-				"image": this.state.uploadedFileLocation
-				}
-			]
+		let destinationURL  = process.env.FLASK_HOST + ":" + process.env.FLASK_PORT + "/predict/default/0"
+		axios({
+		    method: 'post',
+		    url: destinationURL,
+		    timeout: 60 * 4 * 1000, // Let's say you want to wait at least 4 mins
+			data: {
+				"instances": [
+					{
+					"image": this.state.uploadedS3FileName
+					}
+				]
+			}
 		})
-		.then(response => console.log("Successfully sent S3 Bucket URL to Flask API.", response))
-		.catch(error => console.log("Error occurred while sending S3 Bucket URL to Flask API.", error))
+		.then(response => console.log("Successfully sent S3 Bucket URL to Flask API : ", response))
+		.catch(error => console.log("Error occurred while sending S3 Bucket URL to Flask API : ", error))
 	}
 
 	//REACT RENDER FUNCTION
