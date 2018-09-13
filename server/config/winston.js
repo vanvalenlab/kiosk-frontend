a23ad039e9442610e15b47a37dcdde5673e2d739
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-13 13:53:15 -0700

    created file and console loggers for winston

--- server/config/winston.js
+++ server/config/winston.js
@@ -1,12 +1,44 @@
 import * as winston from 'winston';
 
+winston.emitErrs = true;
+
 const logger = winston.createLogger({
+  format: winston.format.printf(info => `${new Date().toISOString()} [${info.level.toUpperCase()}] ${info.message}`),
   transports: [
-    new (winston.transports.Console)({
+    new winston.transports.File({
+      level: 'debug',
+      filename: 'access.log',
+      handleExceptions: true,
       json: true,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false
+    }),
+    new winston.transports.File({
+      level: 'error',
+      filename: 'error.log',
+      handleExceptions: true,
+      json: true,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
       colorize: true
     })
-  ]
+  ],
+  exitOnError: false
 });
+ 
+
+logger.stream = {
+  // eslint-disable-next-line no-unused-vars
+  write: (message, encoding) => {
+    logger.info(message);
+  }
+};
 
 export default logger;
