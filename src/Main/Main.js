0caf819a379b297f7ec6852c6fea9d3b0d7019ae
bchang1213 <brian@podiumbrand.com>
2018-09-18 15:14:47 -0700

    deleted facade, made a main.css file, edited main.css

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Predict from '../Predict/Predict';
 import Logo from '../Logo/Logo';
+import './Main.css';
 
 export default class Main extends React.Component {
   render() {
     return(
-      <main style={{ minHeight: '80vh' }}>
+      <main>
         <Switch>
           <Route exact path='/' component={Predict}/>
           <Route path='/predict' component={Predict}/>
