f38da291be5233757a045f60c6f869ea1f4ab983
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-18 13:18:46 -0700

    using material forms to add models and versions

--- src/Predict/Predict.js
+++ src/Predict/Predict.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
 import axios from 'axios';
 import FileUpload from '../FileUpload/FileUpload';
 
-export default class Predict extends React.Component {
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+
+class Predict extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      file: [],
-      models: null,
-      uploadedS3FileName: null,
-      uploadedFileLocation: null,
+      models: '',
+      model: '',
+      version: '',
       downloadURL: null
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -21,7 +42,7 @@ export default class Predict extends React.Component {
   retrieveModelsVersions() {
     axios.get('/api/getModels')
       .then((response) => {
-        this.models = response.data.models;
+        this.setState({ models: response.data.models});
         console.log(`Got Models: ${JSON.stringify(response.data.models, null, 4)}`);
       })
       .catch((error) => {
@@ -34,10 +55,10 @@ export default class Predict extends React.Component {
     let payload = {
       'imageName': fileName,
       'imageURL': s3Url,
-      'model_name': process.env.MODEL_NAME,
-      'model_version': process.env.MODEL_VERSION
+      'model_name': this.state.model,
+      'model_version': this.state.version
     };
-    console.log(JSON.stringify(payload));
+    console.log(JSON.stringify(payload, null, 4));
 
     axios({
       method: 'post',
@@ -53,11 +74,53 @@ export default class Predict extends React.Component {
       });
   }
 
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <section>
-        <FileUpload onDroppedFile={(fileName, s3Url) => this.predictImage(fileName, s3Url)} />
-      </section>
+      <form className={classes.root} autoComplete="off">
+        { this.state.models !== null ?
+          <FormControl className={classes.formControl}>
+            <Select
+              value={this.state.model}
+              input={<Input name='model' id='model-placeholder' placeholder='' />}
+              onChange={this.handleChange}
+              displayEmpty
+              className={classes.selectEmpty}>
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              { Object.keys(this.state.models).map(m =>
+                <MenuItem value={m} key={m}>{m}</MenuItem>) }
+            </Select>
+          </FormControl>
+          : null }
+        { this.state.model.length > 0 ?
+          <FormControl className={classes.formControl}>
+            <Select
+              value={this.state.version}
+              input={<Input name='version' id='version-placeholder' placeholder='' />}
+              onChange={this.handleChange}>
+              { this.state.models[this.state.model].map(v =>
+                <MenuItem value={v} key={v}>{v}</MenuItem>) }
+            </Select>
+          </FormControl>
+          : null }
+        <div>
+          <FileUpload onDroppedFile={(fileName, s3Url) =>
+            this.predictImage(fileName, s3Url)} />
+        </div>
+      </form>
     );
   }
 }
+
+Predict.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Predict);
