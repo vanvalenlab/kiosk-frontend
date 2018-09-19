ef13c8bc25508575e860a00ba93e8f1784b9bc13
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-19 15:50:32 -0700

    moved multer config into own file, and using different multers based on env.CLOUD

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,9 @@
-import AWS from 'aws-sdk';
 import express from 'express';
 import httpStatus from 'http-status';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import config from '../config/config';
+import multer from '../config/multer';
 import controllers from '../controllers';
 
 const router = express.Router();
-
-AWS.config.update({
-  accessKeyId: config.aws.accessKeyId,
-  secretAccessKey: config.aws.secretAccessKey,
-  region: config.aws.region
-});
-
-const s3 = new AWS.S3();
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: config.aws.bucketName,
-    key: (req, file, cb) => {
-      cb(null, file.originalname);
-    }
-  })
-});
 
 router.route('/health-check')
   .get((req, res) => {
