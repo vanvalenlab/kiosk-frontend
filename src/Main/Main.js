81018a6a263ff4e7ece206f5f91c5440867915e2
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-18 18:58:50 -0700

    NotFound component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Predict from '../Predict/Predict';
+import Train from '../Train/Train';
 import Logo from '../Logo/Logo';
+import NotFound from '../NotFound/NotFound';
 import './Main.css';
 
 export default class Main extends React.Component {
@@ -9,9 +11,10 @@ export default class Main extends React.Component {
     return(
       <main>
         <Switch>
-          <Route exact path='/' component={Predict}/>
+          <Route exact path='/' component={Logo}/>
           <Route path='/predict' component={Predict}/>
           <Route path='/train' component={Logo}/>
+          <Route component={NotFound} />
         </Switch>
       </main>
     );
