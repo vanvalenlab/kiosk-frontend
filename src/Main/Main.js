6676967e9fbb511071f54c2eed97a7c32b4b70cb
bchang1213 <brian@podiumbrand.com>
2018-09-18 19:14:26 -0700

    started grid on main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Predict from '../Predict/Predict';
+import Train from '../Train/Train';
+import NotFound from '../NotFound/NotFound';
 import Logo from '../Logo/Logo';
 import './Main.css';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class Main extends React.Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 600,
+    padding: theme.spacing.unit * 2,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+});
+
+class Main extends React.Component {
   render() {
+    const { classes } = props;
     return(
       <main>
         <Switch>
-          <Route exact path='/' component={Predict}/>
+          <Route exact path='/' component={Logo}/>
           <Route path='/predict' component={Predict}/>
-          <Route path='/train' component={Logo}/>
+          <Route path='/train' component={Train}/>
+          <Route component={NotFound} />
         </Switch>
       </main>
     );
   }
 }
+
+export default withStyles(styles)(Main);
