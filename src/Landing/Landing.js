6c49e396b0ce815bf48c3925cf1f4ad3476bb0e9
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-25 10:09:16 -0700

    css touchups

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -7,20 +7,19 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-  heroUnit: {
-    // backgroundColor: theme.palette.background.paper,
-  },
   heroContent: {
     maxWidth: 600,
     margin: '0 auto',
     padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
   },
   buttonHolder: {
-    flexGrow: 1,
     justifyContent: 'center'
   },
   right: {
     float: 'right'
+  },
+  left: {
+    float: 'left'
   }
 });
 
@@ -30,39 +29,37 @@ class Landing extends React.Component {
 
     return(
       <div>
-        <div className={classes.heroUnit}>
-          <div className={classes.heroContent}>
-            <Typography variant="display4" align="center" color="textPrimary" gutterBottom>
-              DeepCell
-            </Typography>
-            <Typography variant="title" align="center" color="textSecondary" paragraph>
-              Use an existing model to segment images, or train a new model.
-            </Typography>
-          </div>
-          <div className={classes.buttonHolder}>
-            <Grid container spacing={40}>
-              <Grid item md={6} lg={2}>
-                <Button
-                  size="large"
-                  variant="contained"
-                  color="secondary"
-                  className={classNames(classes.button, classes.right)}
-                  href="/predict">
-                  Predict
-                </Button>
-              </Grid>
-              <Grid item md={6} lg={2}>
-                <Button
-                  size="large"
-                  variant="contained"
-                  color="primary"
-                  className={classes.button}
-                  href="/train">
-                  Train
-                </Button>
-              </Grid>
+        <div className={classes.heroContent}>
+          <Typography variant="display4" align="center" color="textPrimary" gutterBottom>
+            DeepCell
+          </Typography>
+          <Typography variant="title" align="center" color="textSecondary" paragraph>
+            Use an existing model to segment images, or train a new model.
+          </Typography>
+        </div>
+        <div className={classes.buttonHolder}>
+          <Grid container spacing={40}>
+            <Grid item xs={6}>
+              <Button
+                size="large"
+                variant="contained"
+                color="secondary"
+                className={classNames(classes.button, classes.right)}
+                href="/predict">
+                Predict
+              </Button>
             </Grid>
-          </div>
+            <Grid item xs={6}>
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                className={classNames(classes.button, classes.left)}
+                href="/train">
+                Train
+              </Button>
+            </Grid>
+          </Grid>
         </div>
       </div>
     );
