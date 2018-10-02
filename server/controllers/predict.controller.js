c3a49609b83d87c9cbba3ef4dd1f435a78214ad4
William Graf <7930703+willgraf@users.noreply.github.com>
2018-10-01 22:31:11 -0700

    single redis connection in config

--- server/controllers/predict.controller.js
+++ server/controllers/predict.controller.js
@@ -1,16 +1,9 @@
 import httpStatus from 'http-status';
-import redis from 'redis';
-import config from '../config/config';
+import client from '../config/redis';
 import logger from '../config/winston';
 
 async function predict(req, res) {
-  const client = redis.createClient(config.redis);
   const redisKey = req.body.imageName;
-  // handle any errors whilst connecting to Redis.
-  client.on('error', (err) => {
-    logger.error(`Error while communicating with Redis: ${err}`);
-    res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
-  });
 
   // set the initial keys/values for redis
   client.hmset([
@@ -24,10 +17,6 @@ async function predict(req, res) {
     if (err) throw err;
     logger.info(`redis.hmset response: ${redisRes}`);
 
-    client.monitor((err, res) => {
-      if (err) throw err;
-      logger.info(`Entering monitoring mode: ${res}`);
-    });
     client.on('monitor', (time, args, raw_reply) => {
       logger.debug(`redis monitor: ${raw_reply}`);
       if (args[1] === redisKey && args[2] === 'output_url' && args[3] !== 'none') {
