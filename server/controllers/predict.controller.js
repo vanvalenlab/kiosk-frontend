ff4968981d78ba1cc73db42f251e9cdd7d4d54d1
William Graf <7930703+willgraf@users.noreply.github.com>
2018-10-02 20:18:05 -0700

    randomize hash name, pass filename as field

--- server/controllers/predict.controller.js
+++ server/controllers/predict.controller.js
@@ -3,36 +3,42 @@ import client from '../config/redis';
 import logger from '../config/winston';
 
 async function predict(req, res) {
-  const redisKey = req.body.imageName;
+  const redisKey = `${req.body.imageName}_${Date.now()}`;
+  try {
+    client.hmset([
+      redisKey,
+      'url', req.body.imageURL,
+      'model_name', req.body.model_name,
+      'model_version', req.body.model_version,
+      'file_name', req.body.imageName,
+      'output_url', 'none',
+      'processed', 'no'
+    ], (err, redisRes) => {
+      if (err) throw err;
+      logger.info(`redis.hmset response: ${redisRes}`);
 
-  // set the initial keys/values for redis
-  client.hmset([
-    redisKey,
-    'url', req.body.imageURL,
-    'model_name', req.body.model_name,
-    'model_version', req.body.model_version,
-    'output_url', 'none',
-    'processed', 'no'
-  ], (err, redisRes) => {
-    if (err) throw err;
-    logger.info(`redis.hmset response: ${redisRes}`);
+      client.on('monitor', (time, args) => {
+        if (args[1] == redisKey) {
+          for (let i = 2; i < args.length; i = i + 2) {
+            if (args[i] === 'output_url' && args[i + 1] !== 'none') {
+              logger.info(`redis key ${args[1]}: ${args[i]} set to: ${args[i + 1]}`);
 
-    client.on('monitor', (time, args) => {
-      if (args[1] == redisKey) {
-        for (let i = 2; i < args.length; i = i + 2) {
-          if (args[i] == 'output_url' && args[i + 1] != 'none') {
-            logger.info(`redis key ${args[1]}: ${args[i]} set to: ${args[i + 1]}`);
-            if (args[i + 1].toLowerCase().startsWith('fail')) {
-              logger.error(`Failed to get output_url due to failure: ${args[i + 1]}`);
-              return res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
-            } else {
-              return res.status(httpStatus.OK).send({ outputURL: args[i + 1] });
+              let outputURL = args[i + 1];
+              if (outputURL.toString().toLowerCase().startsWith('fail')) {
+                logger.error(`Failed to get output_url due to failure: ${outputURL}`);
+                return res.status(httpStatus.SERVICE_UNAVAILABLE).send({ err: outputURL });
+              } else {
+                return res.status(httpStatus.OK).send({ outputURL: outputURL });
+              }
             }
           }
         }
-      }
+      });
     });
-  });
+  } catch (err) {
+    logger.error(`Encountered Error in /predict: ${err}`);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ err: err });
+  }
 }
 
 export default { predict };
