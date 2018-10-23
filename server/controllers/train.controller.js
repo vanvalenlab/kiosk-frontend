4a33f043ef98a5e806a57e3ca9b7d4ca4d8fe5b7
William Graf <7930703+willgraf@users.noreply.github.com>
2018-10-22 21:41:47 -0700

    no more redis monitor

--- server/controllers/train.controller.js
+++ server/controllers/train.controller.js
@@ -4,31 +4,28 @@ import logger from '../config/winston';
 
 async function train(req, res) {
   const redisKey = req.body.imageName;
-
-  // set the initial keys/values for redis
-  client.hmset([
-    redisKey,
-    'url', req.body.imageURL,
-    'optimizer', req.body.optimizer,
-    'field', req.body.fieldSize,
-    'skips', req.body.skips,
-    'epochs', req.body.epochs,
-    'transform', req.body.transform,
-    'normalization', req.body.normalization,
-    'output_url', 'none',
-    'processed', 'no'
-  ], (err, redisRes) => {
-    if (err) throw err;
-    logger.info(`redis.hmset response: ${redisRes}`);
-
-    client.on('monitor', (time, args, raw_reply) => {
-      logger.debug(`redis monitor: ${raw_reply}`);
-      if (args[1] === redisKey && args[2] === 'output_url') {
-        logger.info(`redis key ${args[1]}.output_url set to: ${args[3]}`);
-        return res.status(httpStatus.OK).send({ outputURL: args[3] });
-      }
+  try {
+    // set the initial keys & values for redis
+    client.hmset([
+      redisKey,
+      'url', req.body.imageURL,
+      'optimizer', req.body.optimizer,
+      'field', req.body.fieldSize,
+      'skips', req.body.skips,
+      'epochs', req.body.epochs,
+      'transform', req.body.transform,
+      'normalization', req.body.normalization,
+      'output_url', 'none',
+      'status', 'new'
+    ], (err, redisRes) => {
+      if (err) throw err;
+      logger.info(`redis.hmset response: ${redisRes}`);
+      return res.status(httpStatus.OK).send({ hash: redisKey });
     });
-  });
+  } catch (err) {
+    logger.error(`Encountered Error in /train: ${err}`);
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
 
 export default { train };
