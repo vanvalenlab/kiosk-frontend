5964733577a51af11774efaf05179f26e6626ae2
William Graf <7930703+willgraf@users.noreply.github.com>
2018-11-11 10:28:11 -0800

    redis/expire route

--- server/controllers/redis.controller.js
+++ server/controllers/redis.controller.js
@@ -17,4 +17,26 @@ async function getKey(req, res) {
   }
 }
 
-export default { getKey };
+async function expireHash(req, res) {
+  const redisHash = req.body.hash;
+  const expireTime = req.body.expireIn || 3600;
+  try {
+    client.expire(redisHash, expireTime, (err, value) => {
+      if (err) throw err;
+      if (parseInt(value) == 0) {
+        logger.warning(`Hash "${redisHash}" not found`);
+        return res.status(httpStatus.NOT_FOUND).send({ value });
+      }
+      logger.debug(`Expiring hash ${redisHash} in ${expireTime} seconds: ${value}`);
+      return res.status(httpStatus.OK).send({ value });
+    });
+  } catch (err) {
+    logger.error(`Could not expire hash ${redisHash} with ${expireTime} seconds: ${err}`);
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export default {
+  getKey,
+  expireHash
+};
