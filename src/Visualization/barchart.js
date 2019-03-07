ad887704b2b0706307fa986bcec30c2289024e56
bchang1213 <bchang952@gmail.com>
2019-03-06 17:44:31 -0800

    added displaying values and scaling with values larger than 1000

--- src/Visualization/barchart.js
+++ src/Visualization/barchart.js
@@ -2,10 +2,10 @@ import * as d3 from 'd3';
 
 export function divergingBarChart() {
   var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 600,
+    width = 1600,
     height = 150,
     barHeight = 50,
-    padding = 10,
+    padding = 20,
     dataValue = function(d) { return +d.data; },
     labelValue = function(d) { return d.label; },
     color = [];
@@ -23,9 +23,14 @@ export function divergingBarChart() {
         return { value: dataValue(d), label: labelValue(d) };
       });
 
-      var sumVals = d3.sum(data, function(d) { return d.value; });
+      var sumVals = d3.sum(data, function(d) {
+        return d.value;
+      });
+
+      //creates a continuous scale. https://github.com/d3/d3-scale/blob/master/README.md#scaleLinear
       var barScale = d3.scaleLinear()
-        .domain([0, sumVals])
+        /* the domain of this scale is set to -20, so that the x-values of both rect position and padding can display entirely. */
+        .domain([-20, sumVals])
         .range([0, (width-margin.left-margin.right)]);
 
       var svg = d3.select(this).selectAll('svg').data([data]);
@@ -33,6 +38,15 @@ export function divergingBarChart() {
       gEnter.append('g').attr('class', 'rects')
         .selectAll('.data-rects').data(data).enter()
         .append('rect').attr('class', 'data-rects');
+
+      gEnter.selectAll('text.value')
+        .data(data).enter()
+        .append('text').attr('class', 'metricValue')
+        .attr('x', function(d, i) { return data.slice(0, i).reduce(function (a, d) { return a + barScale(d.value); }, 5) + (padding/2) + 5; })
+        .attr('y', height-margin.bottom-10)
+        .attr('color', 'white')
+        .attr('font-size', 12)
+        .text(function(d) { return d.value; });
 
       gEnter.selectAll('line.legend')
         .data(data).enter()
@@ -56,19 +70,47 @@ export function divergingBarChart() {
       rectG.selectAll('.data-rects').transition()
         .duration(750)
         .attr('x', function(d, i) {
-          //calculate the horizontal x-axis position of the currently selected data rect. d.value is divided by 1.3.
-          return data.slice(0, i).reduce(function (a, d) { return a + barScale(d.value/1.3); }, 1) + (padding / 2);
+          //calculate the horizontal x-axis position of the currently selected data rect. adding + (padding / 2) at the end aligns it with the legend.
+          return data.slice(0, i).reduce(function (a, d) { return a + barScale(d.value); }, 5) + (padding / 2);
         })
         .attr('y', height-margin.bottom-barHeight)
-        //calculate the horizontal width of the currently selected data rect. d.value is divided by 1.3.
-        .attr('width', function(d) { return Math.max(barScale(d.value/1.3) - (padding / 2), 1); })
+        //calculate the horizontal width of the currently selected data rect.
+        .attr('width', function(d) {
+          //if the value of the data is more than 1000, divide it by 10.
+          if(d.value >= 1000){
+            return d.value/10;
+          }
+          else{
+            //Math.max returns either the value of barScale(d.value) - (padding / 2) OR 10. This is because if the first value
+            //is too low, then it defaults to a value of 10 to give it some presence on the bar graph.
+            return Math.max(barScale(d.value) - (padding / 2), 5);
+          }
+        })
         .attr('height', barHeight)
         .attr('fill', function(d, i) { return color[i]; });
 
       // Getting midpoint for legend
       function legendX(d, i) {
-        //calculate the position of the legend text, d.value is divided by 1.3
-        return data.slice(0, i).reduce(function (a, d) { return a + barScale(d.value/1.3); }, (Math.max((barScale(d.value) - (padding / 2)), 1) / 2)) + (padding / 2);
+        /* Original iValue
+        var iValue = Math.max((barScale(d.value) - (padding / 2)), 1) / 2 + (padding / 2);
+        */
+        var iValue = Math.max((barScale(d.value) - (padding / 2)), 10) + (padding / 2);
+        /* if the value of the data is higher than 1000, we scale it back a tad so that it can display nicely with other small values. */
+        if(d.value >= 1000){
+          iValue = Math.max((barScale(d.value/10) - (padding / 2)), 1) + (padding / 2);
+        }
+        //calculate the position of the legend text
+        return data.slice(0, i).reduce(
+          function (a, d) {
+            //due to the scaling we apply to the 1000 or greater values for data rects, we must apply it here to the legend as well as the width and x-position of the rects.
+            if( d.value >= 1000){
+              return a+barScale(d.value/10);
+            }
+            else{
+              return a + barScale(d.value);
+            }
+          },
+          iValue);
       }
 
       var legendLines = g.selectAll('line.legend').data(data, function(d) { return d.label; });
@@ -94,7 +136,7 @@ export function divergingBarChart() {
         .attr('text-anchor', 'middle')
         .attr('x', legendX)
         .attr('y', height-margin.bottom-barHeight-55)
-        .text(function(d) { return d.label;});
+        .text(function(d) { return d.label ;});
       // .text(function(d) { return d3.format(',')(d.value); });
     });
   }
