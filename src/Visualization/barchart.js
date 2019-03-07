94b71169810855038c1e1f6a3be841869442b5a8
bchang1213 <bchang952@gmail.com>
2019-03-06 21:03:38 -0800

    barchart documentation

--- src/Visualization/barchart.js
+++ src/Visualization/barchart.js
@@ -6,14 +6,18 @@ export function divergingBarChart() {
     height = 150,
     barHeight = 50,
     padding = 20,
+    // the '+' symbol here turns the d.data value into a positive integer.
+    // All references to this variable will then become normalized as "positive" in integer value.
     dataValue = function(d) { return +d.data; },
     labelValue = function(d) { return d.label; },
     color = [];
 
+  //This function 'chart' will be the main function that is executed when references are called to
+  // divergingBarChart() in in the higher-order Predict.js component in the function drawChart().
   function chart(selection) {
     //d3 documentation: https://github.com/d3/d3-selection/blob/master/README.md#selection
     //selection represents the selected document element
-    //that is provided when calling using the d3 object.
+    //that is provided in the parameter-argument when calling using the d3 object.
     selection.each(function(data) {
       data = data.map(function(d) {
         //calculate a random color hue.
@@ -22,19 +26,40 @@ export function divergingBarChart() {
         color.push(randomhue);
         return { value: dataValue(d), label: labelValue(d) };
       });
-
+      //Reusable function that sums all values within the parameter-argument data object.
       var sumVals = d3.sum(data, function(d) {
         return d.value;
       });
 
-      //creates a continuous scale. https://github.com/d3/d3-scale/blob/master/README.md#scaleLinear
+      //creates a continuous horizontal scale. https://github.com/d3/d3-scale/blob/master/README.md#scaleLinear
       var barScale = d3.scaleLinear()
         /* the domain of this scale is set to -20, so that the x-values of both rect position and padding can display entirely. */
         .domain([-20, sumVals])
         .range([0, (width-margin.left-margin.right)]);
+      
+      /*This svg var establishes itself as a object capable of controlling an svg tag and giving it data. */
+      var svgController = d3.select(this).selectAll('svg').data([data]);
+      /* gEnter is a variable that means 
+      
+      The most important part of this d3 report is here at the .enter() function.
+      .enter() means "let's enter into the html element that
+      is selected and do stuff inside it."
 
-      var svg = d3.select(this).selectAll('svg').data([data]);
-      var gEnter = svg.enter().append('svg').append('g');
+      Here, it is appending an svg and then appending a g tag right after.
+      https://d3indepth.com/enterexit/#enter
+      */
+      var gEnter = svgController.enter().append('svg').append('g');
+      /* There is currently 1 existing g tag.
+      The below code add yet another g tag under it,
+      and then assigns it a class name of 'rects' 
+      
+      You call .selectAll on any and all child elements within the g tag
+      with className of .data-rects, of which none exist.
+      https://github.com/d3/d3-selection/blob/master/README.md#selectAll
+      In d3, however, you can call .data() after a selection of non-existing elements
+      to immediately create new elements based off of the amount of entries
+      within the data array.
+      */
       gEnter.append('g').attr('class', 'rects')
         .selectAll('.data-rects').data(data).enter()
         .append('rect').attr('class', 'data-rects');
