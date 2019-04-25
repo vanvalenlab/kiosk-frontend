7827f8f642d45282008753d2c4230a5d5c30c51d
William Graf <7930703+willgraf@users.noreply.github.com>
2019-04-25 14:15:57 -0700

    remove all try/catch in express in favor of async error handling.

--- server/controllers/predict.controller.js
+++ server/controllers/predict.controller.js
@@ -10,33 +10,31 @@ async function predict(req, res) {
   if (prefix[prefix.length - 1] === '/') {
     prefix = prefix.slice(0, prefix.length - 1);
   }
-  try {
-    client.hmset([
-      redisKey,
-      'cuts', req.body.cuts,
-      'identity_upload', config.hostname,
-      'input_file_name', req.body.uploadedName,
-      'model_name', req.body.model_name,
-      'model_version', req.body.model_version,
-      'original_name', req.body.imageName,
-      'postprocess_function', req.body.postprocess_function,
-      'status', 'new',
-      'created_at', new Date().toISOString(),
-      'updated_at', new Date().toISOString(),
-      'url', req.body.imageURL
-    ], (err, redisRes) => {
+  client.hmset([
+    redisKey,
+    'cuts', req.body.cuts,
+    'identity_upload', config.hostname,
+    'input_file_name', req.body.uploadedName,
+    'model_name', req.body.model_name,
+    'model_version', req.body.model_version,
+    'original_name', req.body.imageName,
+    'postprocess_function', req.body.postprocess_function,
+    'status', 'new',
+    'created_at', new Date().toISOString(),
+    'updated_at', new Date().toISOString(),
+    'url', req.body.imageURL
+  ], (err, redisRes) => {
+    if (err) {
+      logger.error(`Encountered Error in /predict: ${err}`);
+      return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
+    logger.info(`redis.hmset response: ${redisRes}`);
+    client.lpush(queueName, redisKey, (err, pushRes) => {
       if (err) throw err;
-      logger.info(`redis.hmset response: ${redisRes}`);
-      client.lpush(queueName, redisKey, (err, pushRes) => {
-        if (err) throw err;
-        logger.info(`redis.lpush response: ${pushRes}`);
-        return res.status(httpStatus.OK).send({ hash: redisKey });
-      });
+      logger.info(`redis.lpush response: ${pushRes}`);
+      return res.status(httpStatus.OK).send({ hash: redisKey });
     });
-  } catch (err) {
-    logger.error(`Encountered Error in /predict: ${err}`);
-    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-  }
+  });
 }
 
 export default { predict };
--- server/controllers/train.controller.js
+++ server/controllers/train.controller.js
@@ -10,37 +10,35 @@ async function train(req, res) {
   if (prefix[prefix.length - 1] === '/') {
     prefix = prefix.slice(0, prefix.length - 1);
   }
-  try {
-    // set the initial keys & values for redis
-    client.hmset([
-      redisKey,
-      'epochs', req.body.epochs,
-      'field', req.body.fieldSize,
-      'identity_upload', config.hostname,
-      'input_file_name', `${prefix}/${req.body.imageName}`,
-      'ndim', req.body.ndim,
-      'normalization', req.body.normalization,
-      'optimizer', req.body.optimizer,
-      'skips', req.body.skips,
-      'status', 'new',
-      'updated_at', new Date().toISOString(),
-      'created_at', new Date().toISOString(),
-      'training_type', req.body.trainingType,
-      'transform', req.body.transform,
-      'url', req.body.imageURL
-    ], (err, redisRes) => {
-      if (err) throw err;
-      logger.info(`redis.hmset response: ${redisRes}`);
-      // client.lpush(queueName, redisKey, (err, pushRes) => {
-      //   if (err) throw err;
-      //   logger.info(`redis.lpush response: ${pushRes}`);
-      return res.status(httpStatus.OK).send({ hash: redisKey });
-      // });
-    });
-  } catch (err) {
-    logger.error(`Encountered Error in /train: ${err}`);
-    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-  }
+  // set the initial keys & values for redis
+  client.hmset([
+    redisKey,
+    'epochs', req.body.epochs,
+    'field', req.body.fieldSize,
+    'identity_upload', config.hostname,
+    'input_file_name', `${prefix}/${req.body.imageName}`,
+    'ndim', req.body.ndim,
+    'normalization', req.body.normalization,
+    'optimizer', req.body.optimizer,
+    'skips', req.body.skips,
+    'status', 'new',
+    'updated_at', new Date().toISOString(),
+    'created_at', new Date().toISOString(),
+    'training_type', req.body.trainingType,
+    'transform', req.body.transform,
+    'url', req.body.imageURL
+  ], (err, redisRes) => {
+    if (err) {
+      logger.error(`Encountered Error in /train: ${err}`);
+      return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
+    logger.info(`redis.hmset response: ${redisRes}`);
+    // client.lpush(queueName, redisKey, (err, pushRes) => {
+    //   if (err) throw err;
+    //   logger.info(`redis.lpush response: ${pushRes}`);
+    return res.status(httpStatus.OK).send({ hash: redisKey });
+    // });
+  });
 }
 
 export default { train };
