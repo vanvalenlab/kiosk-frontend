c67229549d7e16fff2aad563866b0540c5f1088f
ajt0ng <ajtong@caltech.edu>
2019-06-24 23:17:07 +0000

    change the copyright end date from 2018 to the current date

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -19,7 +19,13 @@ class Footer extends React.Component {
     return (
       <footer className={classes.footer}>
         <Typography variant='subheading' align='center' color='textSecondary' component='p'>
-          © 2016-2018 The Van Valen Lab at the California Institute of Technology
+          © 201-
+	  <script>
+	    var date = new Date();
+	    var curr_year = date.getFullYear();
+	    document.write(curr_year);
+	  </script>
+	   The Van Valen Lab at the California Institute of Technology
           (Caltech). All rights reserved.
         </Typography>
       </footer>
