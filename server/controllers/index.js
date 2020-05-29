ce6d6997660d85f325b7e78ea67cfea318e77ccf
William Graf <7930703+willgraf@users.noreply.github.com>
2020-05-28 18:37:01 -0700

    remove tracking controller from imports

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,5 @@
 import modelController from './model.controller';
 import predictController from './predict.controller';
-import trackingController from './tracking.controller';
 import redisController from './redis.controller';
 import trainController from './train.controller';
 import uploadController from './upload.controller';
@@ -8,7 +7,6 @@ import uploadController from './upload.controller';
 export default {
   modelController,
   predictController,
-  trackingController,
   redisController,
   trainController,
   uploadController,
