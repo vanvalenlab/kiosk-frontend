9daaf210a5cfc625c8868d5476cff647ca2616dc
William Graf <7930703+willgraf@users.noreply.github.com>
2020-06-15 16:12:23 -0700

    remove commented out ReactGA code. leaving history.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import App from './App/App';
 
 const history = createBrowserHistory();
 
-// Initialize google analytics page view tracking
-// history.listen(location => {
-//   ReactGA.set({ page: location.pathname }); // Update the user's current page
-//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
-// });
-
 ReactDOM.render(
   <Router history={history}>
     <App />
