81fb26dae020da522288ff765719bf973ac4fa9a
William Graf <7930703+willgraf@users.noreply.github.com>
2020-09-16 16:42:05 -0700

    set ReactGA to testMode if NODE_ENV is 'test'

---
+++ src/App/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+import App from './App';
+
+describe('<App/> component tests', () => {
+  beforeEach(() => {
+    ReactGA.testModeAPI.resetCalls();
+  });
+
+  it('should render with a <main> tag', () => {
+    const { container } = render(<App/>, { wrapper: MemoryRouter });
+    const element = container.querySelector('main');
+    expect(element).toBeInTheDocument();
+  });
+});
