718c88030d81558ab4acafcf8b6af5fe9df47e9f
William Graf <7930703+willgraf@users.noreply.github.com>
2020-10-14 14:37:47 -0700

    remove unused Button import

--- src/About/About.js
+++ src/About/About.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
