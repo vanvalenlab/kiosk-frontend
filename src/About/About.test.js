b174268a4c63b9cf4edff888ab179fd2596cd8c8
William Graf <7930703+willgraf@users.noreply.github.com>
2020-10-15 10:14:49 -0700

    fix a test.

--- src/About/About.test.js
+++ src/About/About.test.js
@@ -6,7 +6,7 @@ import About from './About';
 describe('<About/> component tests', () => {
   it('<About/> renders with buttons', () => {
     const { getByText } = render(<About/>);
-    const element = getByText(/deepcell-tf/i);
+    const element = getByText(/DeepCell/i);
     expect(element).toBeInTheDocument();
   });
 });
