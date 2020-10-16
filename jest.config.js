1ad1808d2c9fd7cd2005efbceed006233b6a9893
William Graf <7930703+willgraf@users.noreply.github.com>
2020-10-16 11:07:17 -0700

    fix module mapper to mock image loading

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
   },
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
