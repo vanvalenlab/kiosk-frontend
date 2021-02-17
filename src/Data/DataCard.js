04bff0f5d4fc2168b8cc06eff5037a6dc845dbc0
William Graf <7930703+willgraf@users.noreply.github.com>
2021-02-17 14:10:50 -0800

    Update PropTypes imports.

--- src/Data/DataCard.js
+++ src/Data/DataCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
--- src/FileUpload/FileUpload.js
+++ src/FileUpload/FileUpload.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import CloudUpload from '@material-ui/icons/CloudUpload';
