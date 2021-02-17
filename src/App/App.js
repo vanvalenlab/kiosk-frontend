9334e88ff0a7c755d5e79eed0de45fa7d524daf5
William Graf <7930703+willgraf@users.noreply.github.com>
2021-02-16 18:52:13 -0800

    Use React.lazy instead of loadable.

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import ReactGA from 'react-ga';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
-import { withStyles } from '@material-ui/core/styles';
-import { hot } from 'react-hot-loader';
-import loadable from '@loadable/component';
 
-const About = loadable(() => import('../About/About'));
-const Faq = loadable(() => import('../Faq/Faq'));
-const Footer = loadable(() => import('../Footer/Footer'));
-const NavBar = loadable(() => import('../NavBar/NavBar'));
-const Landing = loadable(() => import('../Landing/Landing'));
-const Predict = loadable(() => import('../Predict/Predict'));
-const Data = loadable(() => import('../Data/Data'));
-const NotFound = loadable(() => import('../NotFound/NotFound'));
+const About = lazy(() => import('../About/About'));
+const Faq = lazy(() => import('../Faq/Faq'));
+const Footer = lazy(() => import('../Footer/Footer'));
+const NavBar = lazy(() => import('../NavBar/NavBar'));
+const Landing = lazy(() => import('../Landing/Landing'));
+const Predict = lazy(() => import('../Predict/Predict'));
+const Data = lazy(() => import('../Data/Data'));
+const NotFound = lazy(() => import('../NotFound/NotFound'));
 
-//If the mode is NOT production, then notify that we are in dev mode.
+// If the mode is NOT production, then notify that we are in dev mode.
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
@@ -62,6 +59,7 @@ class App extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
+      <Suspense fallback={<CircularProgress />}>
         <CssBaseline />
         <NavBar />
         <main className={classes.main}>
@@ -71,11 +69,12 @@ class App extends React.Component {
             <Route path='/faq' component={withTracker(Faq)}/>
             <Route path='/predict' component={withTracker(Predict)}/>
             <Route path='/data' component={withTracker(Data)}/>
-            <SwaggerUI url="/api/swagger.json" />
+            <SwaggerUI url='/api/swagger.json' />
             <Route component={withTracker(NotFound)} />
           </Switch>
         </main>
         <Footer />
+      </Suspense>
       </div>
     );
   }
