460f8a6cb5cae1d3a253efa8d7b1c7dcc6e9d338
William Graf <7930703+willgraf@users.noreply.github.com>
2021-02-18 09:13:32 -0800

    Enable user preferences for themes via prefers-color-scheme.
    Enable the dark theme if the user has a preference to use it.

--- src/App/App.js
+++ src/App/App.js
@@ -3,9 +3,11 @@ import ReactGA from 'react-ga';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import SwaggerUI from 'swagger-ui-react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import 'swagger-ui-react/swagger-ui.css';
 
 const About = lazy(() => import('../About/About'));
@@ -52,7 +54,7 @@ const withTracker = (WrappedComponent, options = {}) => {
   return HOC;
 };
 
-const useStyles = makeStyles(theme => ({ // eslint-disable-line no-unused-vars
+const useStyles = makeStyles({
   root: {
     display: 'flex',
     minHeight: '100vh',
@@ -61,28 +63,43 @@ const useStyles = makeStyles(theme => ({ // eslint-disable-line no-unused-vars
   main: {
     flexGrow: 1,
   }
-}));
+});
 
 export default function App() {
   const classes = useStyles();
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <div className={classes.root}>
-      <Suspense fallback={<CircularProgress />}>
-        <CssBaseline />
-        <NavBar />
-        <main className={classes.main}>
-          <Switch>
-            <Route exact path='/' component={withTracker(Landing)}/>
-            <Route path='/about' component={withTracker(About)}/>
-            <Route path='/faq' component={withTracker(Faq)}/>
-            <Route path='/predict' component={withTracker(Predict)}/>
-            <Route path='/data' component={withTracker(Data)}/>
-            <SwaggerUI url='/api/swagger.json' />
-            <Route component={withTracker(NotFound)} />
-          </Switch>
-        </main>
-        <Footer />
-      </Suspense>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Suspense fallback={<CircularProgress />}>
+          <NavBar />
+          <main className={classes.main}>
+            <Switch>
+              <Route exact path='/' component={withTracker(Landing)}/>
+              <Route path='/about' component={withTracker(About)}/>
+              <Route path='/faq' component={withTracker(Faq)}/>
+              <Route path='/predict' component={withTracker(Predict)}/>
+              <Route path='/data' component={withTracker(Data)}/>
+              <SwaggerUI url='/api/swagger.json' />
+              <Route component={withTracker(NotFound)} />
+            </Switch>
+          </main>
+          <Footer />
+        </Suspense>
+      </div>
+    </ThemeProvider>
   );
 }
