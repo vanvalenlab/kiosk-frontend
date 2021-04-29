b7745caa1ac525dc36d140c49c82ca38ec127b79
William Graf <7930703+willgraf@users.noreply.github.com>
2021-04-29 14:42:46 -0700

    Remove redundant questions:
    - What is DeepCell? (on About)
    - What is the DeepCell Kiosk? (on About)
    - What is deepcell-tf (on About)
    - Where do I get data? (click on data tab)
    - What types of data are supported? (job cards have this info)

--- src/Faq/Faq.js
+++ src/Faq/Faq.js
@@ -36,17 +36,6 @@ export default function Faq() {
 
             <Paper className={classes.paper}>
               <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-                What is DeepCell?
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                DeepCell is a software ecosystem that enables deep learning based biological image analysis in the cloud.
-
-                It consists of several software packages, including the deep learning library <i>deepcell-tf</i>, and a Kubernetes cloud deployment platform, the DeepCell Kiosk.
-              </Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-              <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
                 How do I get started using DeepCell?
               </Typography>
               <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
@@ -60,37 +49,6 @@ export default function Faq() {
                 These models are hosted with <Link href="https://github.com/tensorflow/serving" target="_blank" rel="noopener noreferrer">TensorFlow Serving</Link> and are running on auto-scaling GPUs to minimize cost.
 
                 The servable model files can be found in our <Link href="https://console.cloud.google.com/storage/browser/deepcell-models" target="_blank" rel="noopener noreferrer">public bucket</Link>.
-              </Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-              <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-                What is the DeepCell Kiosk?
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                The DeepCell Kiosk is a turn-key cloud solution for large-scale image processing with deep learning models.
-
-                It is a public software and fully extensible for custom image processing tasks.
-
-                Documentation for the DeepCell Kiosk can be found <Link href="https://deepcell-kiosk.readthedocs.io/en/master/" rel="noopener noreferrer" target="_blank">here</Link>.
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                This website is an implementation of the <Link href="https://github.com/vanvalenlab/kiosk/tree/production" target="_blank" rel="noopener noreferrer">production branch</Link> of the DeepCell Kiosk, and is running on the <Link href="https://cloud.google.com/kubernetes-engine/" target="_blank" rel="noopener noreferrer">Google Kubernetes Engine</Link>.
-              </Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-              <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-                What is <i>deepcell-tf</i>?
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                <Link href="https://github.com/vanvalenlab/deepcell-tf" rel="noopener noreferrer" target="_blank">deepcell-tf</Link> is a TensorFlow/Keras based Python library for training deep learning models for biological image analysis.
-                All models hosted on DeepCell.org have been trained using this library.
-
-                Documentation for the library can be found <Link href="https://deepcell.readthedocs.io/en/master" rel="noopener noreferrer" target="_blank">here</Link>.
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-
               </Typography>
             </Paper>
 
@@ -120,34 +78,6 @@ export default function Faq() {
                 The frontend places jobs in a Redis queue, and the consumers will perform all the work. New jobs will require a new job queue, which are listed in the dropdown list on the <Link href="/predict">Predict page</Link>.
 
                 Custom jobs will also require new consumer with any required pre- and post-processing steps to be defined and deployed in the DeepCell Kiosk with a custom <Link href="https://github.com/roboll/helmfile" rel="noopener noreferrer" target="_blank">helmfile</Link> for the new consumer.
-              </Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-              <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-                Where do I get data?
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                We have uploaded some sample data both on the <Link href="/data">Data page</Link>.
-                The prediction data is meant to be used with the pre-trained models while
-                the training data is available for download for training new models.
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                The training data is also available in <Link href="https://deepcell.readthedocs.io/en/master/API/deepcell.datasets.html" target="_blank" rel="noopener noreferrer">deepcell.datasets</Link> which can be used directly within a Python environment.
-              </Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-              <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-                What types of data are supported?
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                Standard image files are supported (ie. .png, .jpg) as well as .tiff files.
-                Usually we expect around 1000 by 1000 pixel images with no more than 300 objects.
-                Images should be 2D with the exception of data for tracking which should be a 3D stack.
-              </Typography>
-              <Typography variant="body1" align="left" color="textPrimary" gutterBottom>
-                The training data is also available in <Link href="https://github.com/vanvalenlab/deepcell-tf/tree/master/deepcell/datasets" target="_blank" rel="noopener noreferrer">deepcell.datasets</Link> which can be used directly within a Python environment.
               </Typography>
             </Paper>
 
