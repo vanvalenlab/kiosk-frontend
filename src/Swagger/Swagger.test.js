62a3363e0d4a3dac042e54a252be74b2c030bdbc
William Graf <7930703+willgraf@users.noreply.github.com>
2021-05-21 11:06:01 -0700

    Add tests for new Swagger component

---
+++ src/Swagger/Swagger.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Swagger from './Swagger';
+
+describe('<Swagger/> component tests', () => {
+  it('<Swagger/> renders with header', () => {
+    const { container } = render(<Swagger/>);
+    expect(container.firstChild).toHaveClass('swagger-ui');
+  });
+});
