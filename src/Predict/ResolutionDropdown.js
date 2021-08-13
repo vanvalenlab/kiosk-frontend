0ab1e497e2d4da0e9089305c1b6d212c7d8b0bbf
willgraf <7930703+willgraf@users.noreply.github.com>
2021-08-13 08:08:38 -0700

    Replace `ScaleForm` with new `ResolutionDropdown`. (#171)
    * Add new ResolutionDropdown component to convert zoom values to mpp.
    
    Also calculates the scale during the onChange event using modelMpp.
    
    * Replace the ScaleForm with ResolutionDropdown.
    
    remove isAutoRescaleEnabled and add modelResolution state values.
    
    * Turn on scale, add new modelResolution field for modelMpp.
    
    Replaces multiplex body with mesmer monkeypatch
    
    Updates text from "um per pixel" to "um/pixel"
    
    * Add some basic tests for ResolutionDropdown.
    
    * Remove resolution from job card as it is now unnecessary
    
    * Adjust spacing between forms.

---
+++ src/Predict/ResolutionDropdown.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { PropTypes } from 'prop-types';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+export default function ResolutionDropdown({
+  modelMpp = 0.5,
+  scale = 1,
+  onChange = () => {},
+}) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  // values determined from 
+  // https://openwetware.org/wiki/Methods_to_determine_the_size_of_an_object_in_microns
+  // using 0.5 ~= 20x as the starting point
+  const zoomToMpp = {
+    '10x': 1,
+    '20x': 0.5,
+    '40x': 0.25,
+    '60x': 0.1667,
+  };
+
+  return (
+    <FormGroup row>
+      <FormControl>
+        <Select
+          labelId="input-resolution-select-label"
+          id="input-resolution-select"
+          value={scale}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          onOpen={() => setIsOpen(true)}
+          onChange={e => onChange(e.target.value)}
+        >
+          {Object.entries(zoomToMpp).map(([zoom, mpp], i) => {
+            return (
+              <MenuItem value={mpp / modelMpp} key={i}>
+                {zoom} ({mpp} μm/pixel)
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    </FormGroup>
+  );
+}
+
+ResolutionDropdown.propTypes = {
+  scale: PropTypes.number,
+  modelMpp: PropTypes.number,
+  onChange: PropTypes.func,
+};
