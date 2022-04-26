087eab0fd0016e19319fa0eab02be905676af4ed
elaubsch <elaubsch@caltech.edu>
2022-04-26 00:54:17 -0700

    Add dimension order form

--- src/Predict/SegmentationForm.js
+++ src/Predict/SegmentationForm.js
@@ -7,6 +7,33 @@ import Typography from '@mui/material/Typography';
 import ResolutionDropdown from './ResolutionDropdown';
 import ChannelForm from './ChannelForm';
 import jobData from './jobData';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+function DimensionOrderDropdown(props) {
+  const { value, onChange } = props;
+  const orders = ['bxy', 'cxy', 'bxyc', 'cxyb'];
+
+  return (
+    <FormControl variant='standard' fullWidth>
+      <Select
+        size='small'
+        onChange={(e) => onChange(e.target.value)}
+        value={value}
+      >
+        {orders.map((o, i) => (
+          <MenuItem value={o} key={i}>{o}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+DimensionOrderDropdown.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
 
 SegmentationForm.propTypes = {
   selectJobType: PropTypes.element.isRequired,
@@ -20,6 +47,7 @@ export default function SegmentationForm({ selectJobType, setJobForm }) {
   const [selectedChannels, setSelectedChannels] = useState([
     ...Array(channels.length).keys(),
   ]);
+  const [dimensionOrder, setDimensionOrder] = useState('bxy');
   const [scale, setScale] = useState(1);
 
   const updateSelectedChannels = (value, i) => {
@@ -30,8 +58,8 @@ export default function SegmentationForm({ selectJobType, setJobForm }) {
   };
 
   useEffect(() => {
-    setJobForm({ scale, selectedChannels: selectedChannels.join(',') });
-  }, [selectedChannels, scale]);
+    setJobForm({ scale, selectedChannels: selectedChannels.join(','), dimensionOrder });
+  }, [selectedChannels, scale, dimensionOrder]);
 
   return (
     <Grid container>
@@ -51,12 +79,23 @@ export default function SegmentationForm({ selectJobType, setJobForm }) {
             </Grid>
           </Grid>
           <Grid item md={6}>
-            <ChannelForm
-              channels={channels}
-              requiredChannels={requiredChannels}
-              selectedChannels={selectedChannels}
-              onChange={updateSelectedChannels}
-            />
+            <Grid container direction={'column'} spacing={1}>
+              <Grid item lg>
+                <Typography>Dimension Order</Typography>
+                <DimensionOrderDropdown
+                  value={dimensionOrder}
+                  onChange={setDimensionOrder}
+                />
+              </Grid>
+              <Grid item lg>
+                <ChannelForm
+                  channels={channels}
+                  requiredChannels={requiredChannels}
+                  selectedChannels={selectedChannels}
+                  onChange={updateSelectedChannels}
+                />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
